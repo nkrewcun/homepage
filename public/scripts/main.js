50e2f6ac288d030d6e9c39b81645d14de39aaba9
nkrewcun <na.krewcun@gmail.com>
2020-08-10 09:02:14 +0200

    Competences done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,19 @@
 const navbarTogglerBtn = document.querySelector('.navbarToggler');
-const navbarMenu = document.querySelector('.navbarMenu');
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
 navbarTogglerBtn.addEventListener('click', () => {
+    const navbarMenu = document.querySelector('.navbarMenu');
     navbarTogglerBtn.classList.toggle('open');
     navbarMenu.classList.toggle('show');
 });
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.visibility = "visible";
+        scrollToTopBtn.style.opacity = '1';
+
+    } else {
+        scrollToTopBtn.style.opacity = '0';
+        scrollToTopBtn.style.visibility = "hidden";
+    }
+};
